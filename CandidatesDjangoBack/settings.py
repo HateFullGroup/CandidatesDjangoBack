d4f880c5ace71b62a003bbdb2337ceb6919f4e0a
ldx Notebook <lcensies@yandex.ru>
2021-07-05 14:41:50 +0300

    Deleted users app (default Django model will be used)

--- CandidatesDjangoBack/settings.py
+++ CandidatesDjangoBack/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +124,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
