3f1bb1e159dd681d59bcbf55664c174b0ea854d8
ldx Notebook <lcensies@yandex.ru>
2021-07-05 15:44:25 +0300

    Added simple registration api (without validation)

--- CandidatesDjangoBack/settings.py
+++ CandidatesDjangoBack/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CandidatesDjangoBack.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'candidatesdb.sqlite3',
     }
 }
 
