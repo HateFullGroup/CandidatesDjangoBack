199d0d1be4c89ea4eec7e3319781bd463178257e
ldx Notebook <lcensies@yandex.ru>
2021-07-05 15:53:50 +0300

    Added simple login api (without validation)

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 # Create your views here.
 from .serializers import UserSerializer
 
@@ -11,3 +14,22 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.get(username=username)
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
+        return Response({
+            'message': 'success',
+            'detail': 'success'
+        })
