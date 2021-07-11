4d6457975226e79af363a2f6c6b5f3e3a38a15da
Levodex Censies <levodex@outlook.com>
2021-07-11 10:57:14 +0300

    Changed API permissions

--- CandidatesDjangoBack/settings.py
+++ CandidatesDjangoBack/settings.py
@@ -142,14 +142,14 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    # 'PAGE_SIZE': 100
 }
 
 # Permissions:
